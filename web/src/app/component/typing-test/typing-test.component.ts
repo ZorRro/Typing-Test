16af9d85e367ac17f819c940461ce49e0c0f6cd1
ZorRro <debadattameher747@gmail.com>
2020-05-05 15:18:36 +0530

    Working with Backspaces. Need to handle Shift key up

--- web/src/app/component/typing-test/typing-test.component.ts
+++ web/src/app/component/typing-test/typing-test.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit, Output, Input } from "@angular/core";
+import { Component, OnInit, Output, Input, AfterViewInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { ContentService } from 'src/app/service/content.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { EventEmitter } from 'protractor';
+
 
 @Component({
   selector: "app-typing-test",
   templateUrl: "./typing-test.component.html",
   styleUrls: ["./typing-test.component.css"]
 })
-export class TypingTestComponent implements OnInit {
+
+
+export class TypingTestComponent implements OnInit, AfterViewInit {
+
   public test: any;
   public initialContent: string
   public actualContent: string
   public outputContent: string
   private currentIndex = -1;
-  private formattedContent = ""
-
-
+  private formattedContent = ['']
 
   constructor(private route: ActivatedRoute, private contentService: ContentService, private sanitizer: DomSanitizer) { }
+  ngAfterViewInit(): void { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -61,17 +64,32 @@ export class TypingTestComponent implements OnInit {
 
 
   reRenderContent($event) {
+    if ($event.key === 'Shift') return;
     let partial = "", output = "";
 
-    this.currentIndex++;
+    if ($event.key.charCodeAt(0) === 66) {
+      //for backspace
+      this.formattedContent.length > 1 && this.formattedContent.pop()
+      this.currentIndex--;
+      let suffix = this.initial(this.actualContent.substring(this.currentIndex + 1))
+      let prefix = this.formattedContent.join('')
+      output = prefix + suffix;
+      this.outputContent = output
+      return
+    } else {
+      this.currentIndex++
+    }
+
+
     let suffix = this.initial(this.actualContent.substring(this.currentIndex + 1))
-    let prefix = this.currentIndex === 0 ? '' : this.formattedContent
-    if (this.actualContent.charCodeAt(this.currentIndex) === $event.key.charCodeAt(0)) {
+    let prefix = this.formattedContent.join('')
+
+    if (this.actualContent.charAt(this.currentIndex) === $event.key.charAt(0)) {
       partial = this.correct(this.actualContent.substr(this.currentIndex, 1))
     } else {
       partial = this.incorrect(this.actualContent.substr(this.currentIndex, 1))
     }
-    this.formattedContent = prefix + partial;
+    this.formattedContent.push(partial)
     output = prefix + partial + suffix
     this.outputContent = output
   }
